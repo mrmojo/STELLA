794c8db66462aa103051d0c4fb5661928cdb05e6
kuroitenshi <phibo_pg@yahoo.com>
2016-10-03 23:29:27 +0800

    Initial Splash Activity

---
+++ app/src/main/java/com/stellago/stellago/SplashPage.java
@@ -0,0 +1,35 @@
+package com.stellago.stellago;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class SplashPage extends AppCompatActivity {
+
+    /** Duration of wait **/
+    private final int SPLASH_DISPLAY_LENGTH = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_page);
+
+         /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(SplashPage.this,LoginRegisterPage.class);
+                SplashPage.this.startActivity(mainIntent);
+                SplashPage.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+
+}
